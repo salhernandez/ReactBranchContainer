d18786d3c2b3e59eb91f0e25af9f23146d70cfbd
Salvador Hernandez <hernandezg.sal@gmail.com>
2020-10-23 21:52:13 -0700

    Add arguments to image name

--- .Dockerfile
+++ .Dockerfile
@@ -1,8 +1,9 @@
 FROM node:12.16.1
 
 # Will only be used once
-ARG SSH_PRIVATE_KEY
-ARG BRANCH_NAME
+ARG SSH_PRIVATE_KEY=0
+ARG BRANCH_NAME=0
+ARG REPOSITORY_URL=0
 
 # Set working directory
 WORKDIR /app
@@ -16,13 +17,12 @@ RUN ssh-keyscan github.com >> ~/.ssh/known_hosts
 RUN chmod 0600 ~/.ssh/id_rsa
 
 # Clone repository via SSH
-RUN git clone git@github.com:salhernandez/test-react.git
-
-# Remove SSH KEY
-RUN rm -rf ~/.ssh/
+RUN git clone "${REPOSITORY_URL}"
 
 # Set working directory
 # Replace test-react with your project's folder
+
+# this doesn't work properly
 WORKDIR /app/test-react
 
 # get all branches from remote
@@ -32,7 +32,11 @@ RUN git fetch
 RUN git checkout "${BRANCH_NAME}"
 
 RUN npm install
+
 RUN npm install react-scripts
+
+# Remove SSH KEY
+RUN rm -rf ~/.ssh/
 
 # Uses port which is used by the actual application
 EXPOSE 3000
--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# id_rsa private key file path
-ID_RSA_PATH=/c/Users/Sal/.ssh/id_rsa_hernandezgsal
 
-# default branch
-BRANCH_NAME=develop
+REPOSITORY_URL="git@github.com:salhernandez/test-react.git"
+BRANCH_NAME="develop"
+ID_RSA_PATH="/c/Users/Sal/.ssh/id_rsa_hernandezgsal"
+PROJECT_NAME="test-react"
 BUILD_CACHE="--no-cache"
 
 # ./run.sh -b <branch_name> 
@@ -13,9 +13,21 @@ while getopts "b:" arg; do
   esac
 done
 
+IMAGE_NAME="${PROJECT_NAME}/${BRANCH_NAME}:latest"
+
+# export variable so that it can be accessed by docker-compose
+export IMAGE_NAME=$IMAGE_NAME
+
 echo Branch: $BRANCH_NAME
+echo IMAGE_NAME: $IMAGE_NAME
 
 # Create Container
-docker-compose build $BUILD_CACHE --build-arg SSH_PRIVATE_KEY="$(cat /c/Users/Sal/.ssh/id_rsa_hernandezgsal)" --build-arg BRANCH_NAME=$BRANCH_NAME;
+docker-compose build \
+$BUILD_CACHE \
+--build-arg SSH_PRIVATE_KEY="$(cat ${ID_RSA_PATH})" \
+--build-arg BRANCH_NAME=$BRANCH_NAME \
+--build-arg REPOSITORY_URL=$REPOSITORY_URL;
+
 # Run Container
-docker run -it --rm -p 3001:3000 branchcontainer_test_0:latest
+# Bind local machine's port 3001 to container's port 3000
+docker run -it --rm -p 3001:3000 $IMAGE_NAME
